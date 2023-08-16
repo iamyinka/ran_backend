da2fa0c14e2dec79920cd62eaeeadc48a85ad6ee
= <perpetualcoder@gmail.com>
2023-08-16 01:03:09 +0100

    Create models for Partners and Networks

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,25 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Partner(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name",)
+
+
+class Network(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ("name",)
