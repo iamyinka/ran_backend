90093b6ba7104391130c6f19b3c775b2cfcc7e2c
= <perpetualcoder@gmail.com>
2023-09-13 02:12:10 +0100

    Update new server in allowed hosts

--- ran_proj/settings.py
+++ ran_proj/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get('DEBUG') == "True"
 if DEBUG:
     ALLOWED_HOSTS = []
 else:
-    ALLOWED_HOSTS = ["72.167.132.218", "redaid-nigeria.org", "www.redaid-nigeria.org"]
+    ALLOWED_HOSTS = ["92.205.190.133", "redaid-nigeria.org", "www.redaid-nigeria.org"]
 
 
 # Application definition
